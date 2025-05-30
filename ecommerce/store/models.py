b2578b51fba9537cceb5c36816ffbed5cc0f52dd
shekharchaudhari <shekharchaudhari933@gmail.com>
2025-05-30 17:46:40 +0530

    home page changes

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='store/static/categories',default='store/static/categories/default.png')
 
     def __str__(self):
         return self.name
